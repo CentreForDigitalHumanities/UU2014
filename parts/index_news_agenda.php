4a317247af2c70cbc09362442d824177aa26f01d
Simon Welling <simon.welling@gmail.com>
2016-07-11 09:57:16 +0200

    more sticky news homepage

--- parts/index_news_agenda.php
+++ parts/index_news_agenda.php
@@ -3,6 +3,15 @@
 
 					<h2><?php echo $news_title; ?></h2>
 					<?php 
+						$args_sticky = array(
+							'post_type' => 'post',
+							'post__in'  => get_option( 'sticky_posts' ),
+							'ignore_sticky_posts' => 1
+
+						);
+						$newsquery_sticky = new WP_Query( $args_sticky );
+
+
 
 						$newsamount = get_field('uu_options_news_amount', 'option');
 						$newscats = get_field('uu_options_news_frontpage_cat', 'option');
@@ -17,19 +26,29 @@
 							'pagination'    => true,
 							'posts_per_page' => $newsamount,
 							'cat' => $terms,
-							'ignore_sticky_posts'    => false,
+							'post__not_in'  => get_option( 'sticky_posts' ),
 
 						);
 the_field('uu_options_alternative_title_news');
 					$newsquery = new WP_Query( $args );
+					if ( $newsquery_sticky->have_posts() ) {
+							while ( $newsquery_sticky->have_posts() ) {
+									$newsquery_sticky->the_post(); 
+					
+									get_template_part( 'parts/post-loop-frontpage');
+					
+
+				 			} 
+				 	}
+
 					if ( $newsquery->have_posts() ) {
 							while ( $newsquery->have_posts() ) {
 									$newsquery->the_post(); 
 					
-					get_template_part( 'parts/post-loop-frontpage'); ?> 
+									get_template_part( 'parts/post-loop-frontpage');
 					
 
-					<?php } ?>
+				 			} ?>
 						<a class="uu-rss-link" href="/?feed=rss&cat=<?php echo $terms; ?>"><span class="icononly rss"></span>RSS</a>
 					<?php if(get_field('uu_options_alternative_read_more_title_news', 'option')) { $news_readmore_title = get_field('uu_options_alternative_read_more_title_news', 'option'); } else { $news_readmore_title = __('News', 'uu2014'); } ?>	
 					<?php if(get_field('uu_options_frontpage_read_more_links', 'option')) { ?>
@@ -43,7 +62,7 @@ the_field('uu_options_alternative_title_news');
 
 					<?php } ?>
 					
-				</div>
+</div>
 
 				<div class="col-sm-6">
 				<?php if(get_field('uu_options_alternative_title_agenda', 'option')) { $agenda_title = get_field('uu_options_alternative_title_agenda', 'option'); } else { $agenda_title = __('Agenda', 'uu2014'); } ?>
--- parts/index_news_only.php
+++ parts/index_news_only.php
@@ -1,42 +1,64 @@
 <div class="col-sm-8">
 <?php if(get_field('uu_options_alternative_title_news', 'option')) { $news_title = get_field('uu_options_alternative_title_news', 'option'); } else { $news_title = __('News', 'uu2014'); } ?>
 
-	<h2><?php echo $news_title; ?></h2>
-	<?php 
+					<h2><?php echo $news_title; ?></h2>
+					<?php 
+						$args_sticky = array(
+							'post_type' => 'post',
+							'post__in'  => get_option( 'sticky_posts' ),
+							'ignore_sticky_posts' => 1
 
-		$newsamount = get_field('uu_options_news_amount', 'option');
-		$newscats = get_field('uu_options_news_frontpage_cat', 'option');
-		if ($newscats) { 
-			$terms = implode(', ', $newscats);	
-		} else {
-			$terms='';
-		}
-	
-		$args = array(
-			'post_type' => 'post',
-			'pagination'    => true,
-			'posts_per_page' => $newsamount,
-			'cat' => $terms,
-			'ignore_sticky_posts'    => false,
+						);
+						$newsquery_sticky = new WP_Query( $args_sticky );
 
-		);
 
-	$newsquery = new WP_Query( $args );
-	if ( $newsquery->have_posts() ) {
-			while ( $newsquery->have_posts() ) {
-					$newsquery->the_post(); 
-	
-	get_template_part( 'parts/post-loop-frontpage'); ?> 
-	
 
-	<?php } ?>
-		<a class="uu-rss-link" href="/?feed=rss&cat=<?php echo $agendaterms; ?>"><span class="icononly rss"></span>RSS</a>		
-		<?php if(get_field('uu_options_frontpage_read_more_links', 'option')) { ?>
-		<a class="button icon frontpage-read-more" href="<?php if ( function_exists('icl_object_id') ) { echo icl_get_home_url(); } ?>?cat=<?php echo $terms; ?>"><?php echo __('More', 'uu2014') . ' ' . $news_title; ?></a>		
-		<?php } ?>	
-	<?php } else { ?>
+						$newsamount = get_field('uu_options_news_amount', 'option');
+						$newscats = get_field('uu_options_news_frontpage_cat', 'option');
+						if ($newscats) { 
+							$terms = implode(',', $newscats);	
+						} else {
+							$terms='news';
+						}
+					
+						$args = array(
+							'post_type' => 'post',
+							'pagination'    => true,
+							'posts_per_page' => $newsamount,
+							'cat' => $terms,
+							'post__not_in'  => get_option( 'sticky_posts' ),
 
-	<?php get_template_part('includes/template','error'); // WordPress template error message ?>
+						);
+the_field('uu_options_alternative_title_news');
+					$newsquery = new WP_Query( $args );
+					if ( $newsquery_sticky->have_posts() ) {
+							while ( $newsquery_sticky->have_posts() ) {
+									$newsquery_sticky->the_post(); 
+					
+									get_template_part( 'parts/post-loop-frontpage');
+					
 
-	<?php } ?>
+				 			} 
+				 	}
+
+					if ( $newsquery->have_posts() ) {
+							while ( $newsquery->have_posts() ) {
+									$newsquery->the_post(); 
+					
+									get_template_part( 'parts/post-loop-frontpage');
+					
+
+				 			} ?>
+						<a class="uu-rss-link" href="/?feed=rss&cat=<?php echo $terms; ?>"><span class="icononly rss"></span>RSS</a>
+					<?php if(get_field('uu_options_alternative_read_more_title_news', 'option')) { $news_readmore_title = get_field('uu_options_alternative_read_more_title_news', 'option'); } else { $news_readmore_title = __('News', 'uu2014'); } ?>	
+					<?php if(get_field('uu_options_frontpage_read_more_links', 'option')) { ?>
+						<a class="button icon frontpage-read-more" href="<?php if ( function_exists('icl_object_id') ) { echo icl_get_home_url(); } ?>?cat=<?php echo $terms; ?>"><?php echo __('More', 'uu2014') . ' ' . $news_readmore_title; ?></a>		
+					<?php 
+							}
+
+					} else { ?>
+
+					<?php get_template_part('includes/template','error'); // WordPress template error message ?>
+
+					<?php } ?>
 </div>
